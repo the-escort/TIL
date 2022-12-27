cf8ca59ddbdd973975cdc2a5a86cf392282f375c
the-escort <vanitas613@naver.com>
2022-12-27 18:39:44 +0900

    Update Exercise

--- Java/Exercise/src/Exercise3_6.java
+++ Java/Exercise/src/Exercise3_6.java
@@ -3,6 +3,7 @@ public class Exercise3_6 {
 
 	public static void main(String[] args) {
 		int fahrenheit = 100;
+		// C = 5 / 9 X (F - 32)
 		// 소수점 셋째자리에서 반올림
 		float celcius = (int) ((5 / 9f * (fahrenheit - 32)) * 100 + 0.5f) / 100f ;
 		
--- Java/Exercise/src/Exercise4_5.java
+++ Java/Exercise/src/Exercise4_5.java
@@ -2,6 +2,12 @@
 public class Exercise4_5 {
 
 	public static void main(String[] args) {
+
+//		for (int i = 0; i <= 10; i++) {
+//			for (int j = 0; j <= i; j++)
+//				System.out.print("*");
+//			System.out.println();
+
 		int i = 0;
 
 		while (i <= 10) {
@@ -10,9 +16,9 @@ public class Exercise4_5 {
 				System.out.println("*");
 				j++;
 			}
+			
 			System.out.println();
 			i++;
-
 		}
 	}
 
