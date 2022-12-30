28d4b7d224f1594669bf1585200d7d761fe47cf7
the-escort <vanitas613@naver.com>
2022-12-30 21:48:57 +0900

    Update Exercise

--- Java/Exercise/src/Exercise7_2.java
+++ Java/Exercise/src/Exercise7_2.java
@@ -1,11 +1,18 @@
 
-public class Exercise7_1 {
+public class Exercise7_2 {
 
 	public static void main(String args[]) {
 		SutdaDeck deck = new SutdaDeck();
 
+		System.out.println(deck.pick(0));
+		System.out.println(deck.pick());
+		deck.shuffle();
+
 		for (int i = 0; i < deck.cards.length; i++)
 			System.out.print(deck.cards[i] + ",");
+
+		System.out.println();
+		System.out.println(deck.pick(0));
 
 	}
 }
@@ -22,6 +29,28 @@ class SutdaDeck {
 			cards[i] = new SutdaCard(num, isKwang);
 		}
 	}
+
+	void shuffle() {
+		for (int i = 0; i < cards.length; i++) {
+			int j = (int) (Math.random() * cards.length) + 1;
+
+			SutdaCard tmp = cards[j];
+			cards[i] = cards[j];
+			cards[j] = tmp;
+		}
+	}
+
+	SutdaCard pick(int index) {
+		if (index < 0 || index >= CARD_NUM)
+			return null;
+		return cards[index];
+	}
+
+	SutdaCard pick() {
+		int index = (int) (Math.random() * cards.length);
+		return pick(index);
+	}
+
 }
 
 class SutdaCard {
