1f39fddfd76a9535c4132c8b1e24a1cc584e6005
the-escort <vanitas613@naver.com>
2023-02-02 08:08:13 +0900

    Update SpringDI_AOP

--- Spring/SpringDI_AOP/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
+++ Spring/SpringDI_AOP/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -50,15 +50,25 @@ class AppContext {
     }
 
     Object getBean(String key) {
-            return map.get(key);
+        return map.get(key);
+    } // byName
+
+    Object getBean(Class clazz) { // byType
+        for (Object obj : map.values()) {
+            if (clazz.isInstance(obj))
+                return obj;
         }
+        return null;
     }
+}
+
 
 public class Main3 {
     public static void main(String[] args) throws Exception {
         AppContext ac = new AppContext();
 
-        Car car = (Car) ac.getBean("car");
+        Car car = (Car) ac.getBean("car"); // byName
+        Car car2 = (Car) ac.getBean(Car.class); // byType
         Engine engine = (Engine) ac.getBean("engine");
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
