3b4fc7024ba1277ab189393e9b5d3ea3b54481d0
the-escort <vanitas613@naver.com>
2023-02-03 09:07:30 +0900

    Update SpringDI_AOP

--- Spring/SpringDI_AOP/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
+++ Spring/SpringDI_AOP/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -11,6 +11,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.sql.DataSource;
 import java.sql.Connection;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,6 +23,113 @@ public class DBConnectionTest2Test {
     @Autowired
     DataSource ds;
 
+    @Test
+    public void insertUserTest() throws Exception {
+        User user = new User("asdf2", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        deleteAll();
+        int rowCnt = insertUser(user);
+
+        assertTrue(rowCnt == 1);
+    }
+
+    @Test
+    public void selectUserTest() throws Exception {
+        deleteAll();
+        User user = new User("asdf2", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        int rowCnt = insertUser(user);
+        User user2 = selectUser("asdf2");
+
+        assertTrue(user.getId().equals("asdf2"));
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+        deleteAll();
+        int rowCnt = deleteUser("asdf");
+
+        assertTrue(rowCnt == 0);
+
+        User user = new User("asdf2", "1234", "abc", "aaa@aaa.com", new Date(), "fb", new Date());
+        rowCnt = insertUser(user);
+        assertTrue(rowCnt == 1);
+
+        rowCnt = deleteUser(user.getId());
+        assertTrue(rowCnt == 1);
+
+        assertTrue(selectUser(user.getId()) == null);
+    }
+
+    // 매개변수로 받은 사용자 정보로 user_info 테이블을 update하는 메서드
+    public int updateUser(User user) throws Exception {
+        return 0;
+    }
+
+    public int deleteUser(String id) throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info where id = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); // SQL Injection 공격, 성능향상
+        pstmt.setString(1, id);
+//        int rowCnt = pstmt.executeUpdate(); // insert, delete, update
+//        return rowCnt;
+        return pstmt.executeUpdate();
+    }
+
+    public User selectUser(String id) throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "select * from user_info where id = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, id);
+        ResultSet rs = pstmt.executeQuery(); // select
+
+        if (rs.next()) {
+            User user = new User();
+            user.setId(rs.getString(1));
+            user.setPwd(rs.getString(2));
+            user.setName(rs.getString(3));
+            user.setEmail(rs.getString(4));
+            user.setBirth(new Date(rs.getDate(5).getTime()));
+            user.setSns(rs.getString(6));
+            user.setReg_date(new Date(rs.getTimestamp(7).getTime()));
+
+            return user;
+        }
+        return null;
+    }
+
+    private void deleteAll() throws Exception {
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); // SQL Injection 공격, 성능향상
+        pstmt.executeUpdate(); // insert, delete, update
+    }
+
+    // 사용자 정보를 user_info 테이블에 저장하는 메서드
+    public int insertUser(User user) throws Exception {
+        Connection conn = ds.getConnection();
+
+//        insert into user_info (id, pwd, name, email, birth, sns, reg_date)
+//        values ('asdf22', '1234', 'smith', 'aaa@aaa.com', '2022-01-01', 'facebook', now());
+
+        String sql = "insert into user_info values (?, ?, ?, ?, ?, ?, now())";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql); // SQL Injection 공격, 성능향상
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getPwd());
+        pstmt.setString(3, user.getName());
+        pstmt.setString(4, user.getEmail());
+        pstmt.setDate(5, new java.sql.Date(user.getBirth().getTime()));
+        pstmt.setString(6, user.getSns());
+
+        int rowCnt = pstmt.executeUpdate(); // insert, delete, update
+
+        return rowCnt;
+    }
     @Test
     public void springJdbcConnectionTest() throws Exception {
 //        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
