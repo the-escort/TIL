eb1f627075cbb9bcebf1ac616471db510e7f0a62
the-escort <vanitas613@naver.com>
2023-02-10 23:49:10 +0900

    Update MyBatis

--- Spring/MyBatis/src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ Spring/MyBatis/src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -37,4 +37,6 @@ public interface BoardDao {
     List<BoardDto> searchSelectPage(SearchCondition sc) throws Exception;
 
     int searchResultCnt(SearchCondition sc) throws Exception;
+
+    int updateCommentCnt(Integer bno, int cnt);
 }
--- Spring/MyBatis/src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
+++ Spring/MyBatis/src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -74,4 +74,13 @@ public class BoardDaoImpl implements BoardDao {
     public int searchResultCnt(SearchCondition sc) throws Exception {
         return session.selectOne(namespace + "searchResultCnt", sc);
     } // T selectOne(String statement
+
+    @Override
+    public int updateCommentCnt(Integer bno, int cnt) {
+        Map map = new HashMap();
+        map.put("cnt", cnt);
+        map.put("bno", bno);
+
+        return session.update(namespace + "updateCommentCnt", map);
+    }
 }
